be9a565d951ec0194a8dcea9a47405bb1f7d926c
mohamedKhaled655 <mohamedkh65536@gmail.com>
2025-01-11 18:31:38 +0200

    update logic in server to handle some problems in json

--- server/src/db/DAO.java
+++ server/src/db/DAO.java
@@ -13,16 +13,14 @@ import org.apache.derby.jdbc.ClientDriver;
 public class DAO {
 
     private Connection connection;
+    private static boolean isConnected = false;
 
     public DAO() {
-        connect();
-    }
-
-    public void connect() {
         try {
             if (connection == null || connection.isClosed()) {
                 connection = DriverManager.getConnection("jdbc:derby://localhost:1527/User", "root", "root");
                 System.out.println("Database connected successfully.");
+                isConnected = true;
             }
         } catch (SQLException e) {
             Logger.getLogger(DAO.class.getName()).log(Level.SEVERE, null, e);
@@ -41,6 +39,12 @@ public class DAO {
     }
 
     public static boolean registerForUser(String username, String password) throws SQLException {
+
+        if (isUsernameTaken(username)) {
+            System.out.println("Username already exists.");
+            return false;
+        }
+
         int result = 0;
         DriverManager.registerDriver(new ClientDriver());
         Connection con = DriverManager.getConnection("jdbc:derby://localhost:1527/Users", "root", "root");
@@ -48,57 +52,37 @@ public class DAO {
         ps.setString(1, username);
         ps.setString(2, password);
         result = ps.executeUpdate();
-        con.close();
+        //con.close();
         ps.close();
-        if (result > 0) {
-            return true;// return respone to ui if positive added succ else -neg alert error 
-        } else {
-            return false;
-        }
+        return result > 0;
 
     }
 
-    public static boolean loginForUser(String username, String password) throws SQLException {
-        int result = 0;
+    public boolean loginForUser(String username, String password) throws SQLException {
+
         DriverManager.registerDriver(new ClientDriver());
         Connection con = DriverManager.getConnection("jdbc:derby://localhost:1527/Users", "root", "root");
-        PreparedStatement ps = con.prepareStatement("SELECT * FROM users WHERE username = ? AND password = ?", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        PreparedStatement ps = con.prepareStatement("SELECT * FROM Users WHERE username = ? AND password = ?", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
         ps.setString(1, username);
         ps.setString(2, password);
         ResultSet resultSet = ps.executeQuery();
-        return resultSet.next();
+        resultSet.next();
+
+        return resultSet.getInt(1) > 0;
 
     }
-    /*  
-    public boolean loginUser(String username, String password) {
-        String query = "SELECT * FROM Users WHERE username = ? AND password = ?";
-        try (PreparedStatement statement = connection.prepareStatement(query)) {
-            statement.setString(1, username);
-            statement.setString(2, password);
-            ResultSet resultSet = statement.executeQuery();
-            return resultSet.next();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
 
-  public boolean registerUser(String username, String password) {
-        String query = "INSERT INTO Users (username, password,) VALUES (?, ?)";
-        try (PreparedStatement statement = connection.prepareStatement(query)) {
-            statement.setString(1, username);
-            statement.setString(2, password);
-            
-            statement.executeUpdate();
-            return true;
-        } catch (SQLIntegrityConstraintViolationException e) {
-            System.out.println("Username already exists.");
-            return false;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
+    private static boolean isUsernameTaken(String username) throws SQLException {
+
+        DriverManager.registerDriver(new ClientDriver());
+        Connection con = DriverManager.getConnection("jdbc:derby://localhost:1527/Users", "root", "root");
+        PreparedStatement ps = con.prepareStatement("SELECT COUNT(*) FROM users WHERE username = ?", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        ps.setString(1, username);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        return rs.getInt(1) > 0;
+
     }
-     */
 
 }
