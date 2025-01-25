7e6b2cb183a81a2d750d6e0c3c14ea17f9a86227
Tasneem <tasneemmohamed20@gmail.com>
2025-01-25 02:38:49 +0200

    chart updated

--- server/src/db/DAO.java
+++ server/src/db/DAO.java
@@ -152,6 +152,21 @@ public class DAO {
 
         return result > 0;
     }
+        
+    public static Vector<String> getInGameUsers() throws SQLException {
+        Vector<String> inGameUsers = new Vector<>();
+        DriverManager.registerDriver(new ClientDriver());
+        Connection con = DriverManager.getConnection("jdbc:derby://localhost:1527/Users", "root", "root");
+        PreparedStatement ps = con.prepareStatement("SELECT username FROM users WHERE status = 'ingame'", 
+            ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        ResultSet res = ps.executeQuery();
+        while (res.next()) {
+            inGameUsers.add(res.getString("username"));
+        }
+        con.close();
+        ps.close();
+        return inGameUsers;
+    }
 
 }
 
--- server/src/serverUI/FXMLDocumentController.java
+++ server/src/serverUI/FXMLDocumentController.java
@@ -43,10 +43,10 @@ public class FXMLDocumentController implements Initializable {
     Server server;
     Thread thread;
     boolean isRunning = false;
-    Series<String, Integer> onlinePlayers = new Series<>(); // Use Series instead of XYSeries
-    Series<String, Integer> offlinePlayers = new Series<>(); // Use Series instead of XYSeries
+    Series<String, Integer> onlinePlayers = new Series<>(); 
+    Series<String, Integer> offlinePlayers = new Series<>(); 
+    Series<String, Integer> inGamePlayers = new Series<>();
     private Thread chartUpdaterThread;
-    private final Object chartLock = new Object();
 
     @FXML
     private Button startButton;
@@ -64,89 +64,31 @@ public class FXMLDocumentController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        if (barChart != null) {
-            barChart.getStylesheets().add(getClass().getResource("chart.css").toExternalForm());
-        }
-
         onlinePlayers.setName("Online");
         offlinePlayers.setName("Offline");
-
-        Platform.runLater(() -> {
-            if (onlinePlayers.getNode() != null) {
-                onlinePlayers.getNode().getStyleClass().add("onlinePlayers");
-            }
-            if (offlinePlayers.getNode() != null) {
-                offlinePlayers.getNode().getStyleClass().add("offlinePlayers");
-            }
-        });
+        inGamePlayers.setName("In Game");
     }
 
-//    @FXML
-    // private void handleStartButton(ActionEvent event) {
-        
-    //     try {
-    //         if (server == null) {
-    //             server = new Server();
-    //         }
-    //         isRunning = true;
-    //         updateChart();
-    //         thread = new Thread(() -> {
-    //             server.startServer();
-                
-    //             Platform.runLater(() -> {
-    //                 chartUpdaterThread = new Thread(() -> {
-    //                     System.out.println("chartUpdaterThread is Alive");
-    //                     while (isRunning) {
-    //                         Platform.runLater(this::updateChart);
-    //                         try {
-    //                             System.out.println("i'm here");
-    //                             Thread.sleep(5000); 
-    //                         } catch (InterruptedException ex) {
-                               
-    //                             break;
-    //                         }
-    //                     }
-    //                 });
-    //                 chartUpdaterThread.setDaemon(true);
-    //                 chartUpdaterThread.start();
-    //             });
-    //         });
-    //         thread.start();
-    //         thread.sleep(1000);
-            
-
-            
-    //         System.out.println("Server Started");
-    //         startButton.setDisable(true);
-    //         endButton.setDisable(false);
-
-    //     } catch (Exception ex) {
-    //         ex.printStackTrace();
-    //     }
-    // }
-
     @FXML
-private void handleStartButton(ActionEvent event) {
+    private void handleStartButton(ActionEvent event) {
     try {
         if (server == null) {
             server = new Server();
         }
         isRunning = true;
         updateChart();
-        // Create and start server thread
         thread = new Thread(() -> {
             server.startServer();
         });
         thread.setDaemon(true);
         thread.start();
 
-        // Create and start chart updater thread independently
         chartUpdaterThread = new Thread(() -> {
             System.out.println("chartUpdaterThread is Alive");
             while (isRunning) {
                 Platform.runLater(this::updateChart);
                 try {
-                    System.out.println("i'm here");
+//                    System.out.println("i'm here");
                     Thread.sleep(5000);
                 } catch (InterruptedException ex) {
                     break;
@@ -154,10 +96,7 @@ private void handleStartButton(ActionEvent event) {
             }
         });
         chartUpdaterThread.setDaemon(true);
-        chartUpdaterThread.start();
-
-        // Initial chart update
-        
+        chartUpdaterThread.start();        
         
         System.out.println("Server Started");
         startButton.setDisable(true);
@@ -169,58 +108,64 @@ private void handleStartButton(ActionEvent event) {
 }
 
     private void updateChart() {
-        System.out.println("Updating chart...");
+//        System.out.println("Updating chart...");
 
-        // Create new series every time
         Series<String, Integer> online = new Series<>();
-        online.setName("Online");
         Series<String, Integer> offline = new Series<>();
+        Series<String, Integer> inGame = new Series<>();
+        online.setName("Online");
         offline.setName("Offline");
+        inGame.setName("In Game");
 
-        updateChartData(online, offline);
+        updateChartData(online, offline, inGame);
 
         ObservableList<XYChart.Series<String, Integer>> newData = FXCollections.observableArrayList();
-        newData.addAll(online, offline);
+        newData.addAll(online, offline, inGame);
         barChart.setData(newData);
     }
     
-    private void updateChartData(Series<String, Integer> onlinePlayers, Series<String, Integer> offlinePlayers) {
+    private void updateChartData(Series<String, Integer> onlinePlayers, Series<String, Integer> offlinePlayers, Series<String, Integer> inGamePlayers) {
         try {
             Vector<String> onlineUsers = DAO.getAllInlineUsers();
             Vector<String> allUsers = DAO.getAllUsers();
+            Vector<String> inGameUsers = DAO.getInGameUsers();
+            
             int onlineCount = onlineUsers.size();
-            int offlineCount = allUsers.size() - onlineCount;
+            int inGameCount = inGameUsers.size();
+            int offlineCount = allUsers.size() - (onlineCount + inGameCount);
     
             onlinePlayers.getData().add(new XYChart.Data<>("Users", onlineCount));
             offlinePlayers.getData().add(new XYChart.Data<>("Users", offlineCount));
+            inGamePlayers.getData().add(new XYChart.Data<>("Users", inGameCount));
         } catch (SQLException ex) {
             Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
-    
 
     @FXML
     private void handleEndButton(ActionEvent event) {
         stop();
     }
 
-    public void stop() {
+    public synchronized void stop() {
         try {
             isRunning = false;
 
-            if (chartUpdaterThread != null) {
-                chartUpdaterThread.interrupt();
-            }
-
-            if (thread != null) {
-                thread.interrupt();
-            }
-
             if (server != null) {
                 server.stopServer();
+                server = null;
             }
-           
+            
+            if (thread != null) {
+                thread.interrupt();
+                thread = null;
+            }
+            
+            if (chartUpdaterThread != null) {
+                chartUpdaterThread.interrupt();
+                chartUpdaterThread = null;
+            }
+
             barChart.getData().clear();
             Platform.runLater(() -> {
             FadeTransition fadeOut = new FadeTransition(Duration.millis(1000), barChart);
@@ -229,6 +174,7 @@ private void handleStartButton(ActionEvent event) {
             fadeOut.setOnFinished(e -> {
                 onlinePlayers = null;
                 offlinePlayers = null;
+                inGamePlayers = null;
                 barChart.setOpacity(1.0);
             });
             fadeOut.play();                
